e519f7d0b3ae9188192907ad80a7f89297ed780b
Devsaider <sebasmoraresu@gmail.com>
2021-04-18 21:39:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,5 +17,5 @@ var coordenadas = () => {
 }
 
 btnyes.addEventListener('click', () => {
-    alert('Ya sabia que querias ser mi novia');
+    alert('¿En serio? ¡Sabía que querías ser mi novi@!');
 });
